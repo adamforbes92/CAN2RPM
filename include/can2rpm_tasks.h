f27a4c2b8cd88d5324e7b4d22fd62f3ef0a768e6
Forbes-Automotive <34219819+adamforbes92@users.noreply.github.com>
2026-09-12 15:20:48 +0100

    Delete include directory

--- include/can2rpm_tasks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Starts all background RTOS tasks.
-// Call once from setup() after all peripherals are initialised.
-void startTasks();
-
-// Temporarily pause/resume output update tasks during manual sweeps.
-void suspendOutputTasks();
-void resumeOutputTasks();
